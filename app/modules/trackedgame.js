695519302eacd6f5466746334776acc1bd7a9eb1
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-03-08 20:58:09 +0900

    Updated track game skeleton

--- app/modules/trackedgame.js
+++ app/modules/trackedgame.js
@@ -23,24 +23,20 @@ function(namespace, Backbone, Player, Game) {
 	// ROUTER
 	//
 	TrackedGame.Router = Backbone.Router.extend({
-		// Create a router to map faux-URLs to actions.
-		// Faux-URLs come from Backbone.history.navigate or hashes in a URL from a followed link.
 		routes : {
 			"track/:gameId": "trackGame",
 		},
 		trackGame: function (gameId) {
 			var trackedGame = new Game.Model({id: gameId});
 			trackedGame.fetch();
-		
+			//This router won't get called again unless manually refreshed,
+			//so pass the views as much data as they need.
 			var myLayout = app.router.useLayout("tracked_game");
 			myLayout.setViews({
-				".header": new TrackedGame.Views.Header({trackedGame:trackedGame}),
 				".scoreboard": new TrackedGame.Views.Scoreboard({trackedGame:trackedGame}),
-				".player_buttons": new TrackedGame.Views.PlayerButtons({trackedGame:trackedGame}),
-				".action_buttons": new TrackedGame.Views.ActionButtons({trackedGame:trackedGame}),
-				".metadata": new TrackedGame.Views.Metadata({trackedGame:trackedGame})
+				".player_area": new TrackedGame.Views.PlayerArea({trackedGame:trackedGame}),
+				".action_area": new TrackedGame.Views.ActionArea({trackedGame:trackedGame, show_main:true})
 			});
-			
 			
 			myLayout.render(function(el) {$("#main").html(el);});
 		},
@@ -50,47 +46,89 @@ function(namespace, Backbone, Player, Game) {
 	//
 	// VIEWS
 	//
-	TrackedGame.Views.Header = Backbone.View.extend({
-		template: "trackedgame/header",
-		serialize: function() {
-			return this.options.trackedGame.toJSON();
-		},
-		initialize: function() {this.options.trackedGame.bind("change", function() {this.render();}, this);}
-	});
 	TrackedGame.Views.Scoreboard = Backbone.View.extend({
 		template: "trackedgame/scoreboard",
 		serialize: function() {return this.options.trackedGame.toJSON();},
 		initialize: function() {this.options.trackedGame.bind("change", function() {this.render();}, this);}
 	});
-	TrackedGame.Views.PlayerButtons = Backbone.View.extend({
-		template: "trackedgame/playerbuttons",
+	TrackedGame.Views.PlayerArea = Backbone.View.extend({
+		template: "trackedgame/player_area",
 		serialize: function() {return this.options.trackedGame.toJSON();},
+		//Get what we need from this.options.trackedGame and set the player buttons
 		initialize: function() {this.options.trackedGame.bind("change", function() {this.render();}, this);}
 	});
-	TrackedGame.Views.ActionButtons = Backbone.View.extend({
-		template: "trackedgame/actionbuttons",
-		serialize: function() {return this.options.trackedGame.toJSON();},
-		initialize: function() {this.options.trackedGame.bind("change", function() {this.render();}, this);},
+	TrackedGame.Views.ActionArea = Backbone.View.extend({
+		template: "trackedgame/action_area",
 		events: {
-			"click .drop": "drop",
+			"click .undo": "undo",
+			"click .misc": "misc",
 			"click .score": "score",
-			"click .timeout": "timeout"
+			"click .completion": "completion",
+			"click .throwaway": "throwaway",
+			"click .dropped_pass": "dropped_pass",
+			"click .defd_pass": "defd_pass",
+			"click .unknown_turn": "unknown_turn",
+			"click .timeout": "timeout",
+			"click .end_of_period": "end_of_period",
+			"click .injury": "injury"
 		},
-		drop: function(ev){
-			console.log("TODO: Drop")
+		undo: function(ev){
+			console.log("TODO: undo")
+		},
+		misc: function(ev){
+			if (this.options.show_main) {
+				this.$('.alternate_action').show();
+				this.$('.main_action').hide();
+				this.options.show_main=false;
+			}
+			else {
+				this.$('.main_action').show();
+				this.$('.alternate_action').hide();
+				this.options.show_main=true;
+			}
 		},
 		score: function(ev){
-			console.log("TODO: Score")
+			console.log("TODO: score")
+		},
+		completion: function(ev){
+			console.log("TODO: completion")
+		},
+		throwaway: function(ev){
+			console.log("TODO: throwaway")
+		},
+		dropped_pass: function(ev){
+			console.log("TODO: dropped_pass")
+		},
+		defd_pass: function(ev){
+			console.log("TODO: defd_pass")
+		},
+		unknown_turn: function(ev){
+			console.log("TODO: unknown_turn")
 		},
 		timeout: function(ev){
 			console.log("TODO: timeout")
-		}
-	});
-	TrackedGame.Views.Metadata = Backbone.View.extend({
-		template: "trackedgame/metadata",
-		serialize: function() {return this.options.trackedGame.toJSON();},
+		},
+		end_of_period: function(ev){
+			console.log("TODO: end_of_period")
+		},
+		injury: function(ev){
+			console.log("TODO: injury")
+		},
+		
+		render: function(layout) {
+			var view = layout(this);
+			return view.render().then(function(el) {
+				if (this.options.show_main) {
+					this.$('.alternate_action').hide();
+					this.$('.main_action').show();
+				}
+				else {
+					this.$('.alternate_action').show();
+					this.$('.main_action').hide();
+				}
+			});
+		},
 		initialize: function() {this.options.trackedGame.bind("change", function() {this.render();}, this);}
 	});
-	
 	return TrackedGame;
 });
