1a4370fa03987a93fa67d58a1ef1e2aa22ff4401
Mark Liu <markwayneliu@gmail.com>
2012-03-10 01:52:25 -0600

    Added a view to render a game status bar. It is different depending on whether or not the game is part of a tournament. Added a simple div template because each view requires a template, even if all it needs is a new div.

--- app/modules/game.js
+++ app/modules/game.js
@@ -6,11 +6,12 @@ define([
 
   // Modules
   "modules/navigation",
+  "modules/title",
   
   // Plugins
   "use!plugins/backbone.layoutmanager"
 ],
-function(namespace, Backbone, Navigation) {
+function(namespace, Backbone, Navigation, Title) {
 	var app = namespace.app;
 	var Game = namespace.module();
 	
@@ -84,6 +85,7 @@ function(namespace, Backbone, Navigation) {
 			var myLayout = app.router.useLayout("nav_content");// Get the layout from a layout cache.
 			// Layout from cache might have different views set. Let's (re-)set them now.
 			myLayout.view(".navbar", new Navigation.Views.Navbar({href: "#newgame", name: "New"}));
+			myLayout.view(".titlebar", new Title.Views.Titlebar({title: "Games"}));
 			myLayout.view(".content", new Game.Views.List ({collection: app.games}));//pass the List view a collection of (fetched) games.
 			myLayout.render(function(el) {$("#main").html(el);});// Render the layout, calling each subview's .render first.
 		},
@@ -92,7 +94,14 @@ function(namespace, Backbone, Navigation) {
 			if (!app.games) {app.games = new Game.Collection();}//Will create an empty collection.
 			if (!app.games.get(gameId)) {app.games.add( [{id: parseInt(gameId)}] );}//Insert this game into the collection.
 			game = app.games.get(gameId);
-			game.fetch();
+			game.fetch({success: function (model, response) {
+                    // After the game has been fetched, render the nav-bar so the back button says tournament
+                    // if the game is part of a tournament
+                    var myLayout = app.router.useLayout("div");
+                    myLayout.view("div", new Game.Views.Titlebar({model: game}));
+                    myLayout.render(function(el) {$(".titlebar").html(el)});
+                }
+            });
 			//TODO: Get some game stats.
 			var myLayout = app.router.useLayout("nav_detail_list");// Get the layout. Has .navbar, .detail, .list_children
 			myLayout.setViews({
@@ -100,6 +109,7 @@ function(namespace, Backbone, Navigation) {
 				".detail": new Game.Views.Detail( {model: game}),
 				".list_children": new Game.Views.Multilist({model: game})//TODO: add stats					
 			});
+			myLayout.view(".titlebar", new Game.Views.Titlebar({model: game}));
 			myLayout.render(function(el) {$("#main").html(el);});// Render the layout, calling each subview's .render first.
 		}
 	});
@@ -113,6 +123,34 @@ function(namespace, Backbone, Navigation) {
 		tagName: "li",//Creates a li for each instance of this view. Note below that this li is inserted into a ul.
 		serialize: function() {return this.model.toJSON();} //render looks for this to manipulate model before passing to the template.
 	});
+	Game.Views.Titlebar = Backbone.View.extend({
+        template: "layouts/div", //Use an empty template for rendering since it's just a wrapper around Title.Views.Titlebar
+        render: function(layout) { //Render the title for the game depending on if it is in a tournament or not
+            var view = layout(this);
+            var tournamentId = this.model.get('tournament_id');
+            var left_btn_href = "#games";
+            var left_btn_txt = "Games";
+            if (tournamentId != null) {
+                //Set the back button to point to this game's tournament if it is part of a tournament
+                left_btn_href = "#tournaments/" + tournamentId;
+                left_btn_txt = "Tournament";
+            }
+            this.setViews(
+                {"div": new Title.Views.Titlebar({
+                    title: "Game", 
+                    left_btn_href: left_btn_href, 
+                    left_btn_class: "back", 
+                    left_btn_txt: left_btn_txt
+                })
+            });
+            return view.render(function(el) { debugger });
+        },
+		initialize: function() {
+    		this.bind("change", function() {
+      			this.render();
+    		}, this);
+  		}
+    });
 	Game.Views.List = Backbone.LayoutManager.View.extend({
 		template: "games/list",
 		className: "games-wrapper",
