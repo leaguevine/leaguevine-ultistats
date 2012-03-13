e3a973012c2e223661da2b3fe928f2e705e04e15
Mark Liu <markwayneliu@gmail.com>
2012-03-13 18:29:46 -0500

    Added classes to the navigation bar that show which part of the app the user is currently in.

--- app/modules/navigation.js
+++ app/modules/navigation.js
@@ -16,9 +16,38 @@ function(namespace, Backbone, Game) {
     	//className: "navbar-wrapper",
     	href: "",
     	name: "",
+        currently_viewed: "",
 	    render: function(layout) {
 	    	var view = layout(this);
-	    	return view.render({href: this.options.href, name: this.options.name});
+
+            // Set the classes for the currently viewed navigation button
+            var tournaments_class = "";
+            var teams_class = "";
+            var game_class = "";
+            var settings_class = "";
+            var currently_viewed = "currently_viewed";
+            var fragment = Backbone.history.fragment;
+            if (fragment.indexOf("tournament") != -1) { // If the current URL is a tournament URL
+                tournaments_class = currently_viewed;
+            } 
+            else if (fragment.indexOf("team") != -1) { // If the current URL is a team URL
+                teams_class = currently_viewed;
+            } 
+            else if (fragment.indexOf("game") != -1) {
+                game_class = currently_viewed;
+            }
+            else if (fragment.indexOf("settings") != -1) {
+                settings_class = currently_viewed;
+            }
+        
+	    	return view.render({
+                href: this.options.href, 
+                name: this.options.name, 
+                tournaments_class: tournaments_class,
+                teams_class: teams_class,
+                game_class: game_class,
+                settings_class: settings_class,
+            });
 	    },
 	    initialize: function() {
     		this.bind("change", function() {
