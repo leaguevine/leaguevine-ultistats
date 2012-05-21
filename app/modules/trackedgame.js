083b7e81b7df9fa71865ee5a25f387b68c6ed75c
Chadwick Boulay <chadwick.boulay@gmail.com>
2012-05-22 02:18:55 +0900

    Put a name on the "PLAYER Throws a:" label.

--- app/modules/trackedgame.js
+++ app/modules/trackedgame.js
@@ -15,7 +15,6 @@ define([
 
 /*
  * Chad's TODO list:
- * -Fix the "PLAYER" Throws a: prompt
  * -Use a single (persistent) button to rotate through game/sub1/sub2. Maybe in the bottom right corner like a page turn?
  * -enable/disable buttons depending on state
  * -Player model needs a function that returns its formatted name from its attributes.
@@ -796,7 +795,14 @@ function(require, namespace, Backbone) {
 		},
 		render: function(layout) {
 			var view = layout(this);
-			return view.render({per_num: this.model.get("period_number")}).then(function(el) {
+			var pl_string = "";
+			var pl_id = this.model.get("player_in_possession_id");
+			var team_ix = this.model.get("team_in_possession_ix");
+			if (pl_id){
+				var pl_model = _.find(this.model.get("onfield_" + team_ix).pluck("player"), function(pl_obj){return pl_obj.id == pl_id;});
+                if (pl_model != undefined) {pl_string = pl_model.first_name[0] + ". " + pl_model.last_name + " ";}
+			}
+			return view.render({player_string: pl_string, per_num: this.model.get("period_number")}).then(function(el) {
 				this.show_action_buttons();
                 this.show_player_name();
                 this.model.setButtonHeight();
