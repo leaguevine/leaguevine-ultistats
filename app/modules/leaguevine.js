44aa664899e390a12407800db749c50a6f2169a9
Mark Liu <markwayneliu@gmail.com>
2012-07-20 15:27:53 -0400

    Fixed a javascript bug. Fixed some css. Everything seems to be working now and ready to deploy.

--- app/modules/leaguevine.js
+++ app/modules/leaguevine.js
@@ -122,7 +122,7 @@ function(app, Backbone) {
         is_logged_in: function() {//Returns true if the user is logged in and false if not
 			if (!this.token) {
 				var stored_api = JSON.parse(localStorage.getItem("auth_object"));
-				_extend(this, stored_api);
+				_.extend(this, stored_api);
 			}
             return (this.token !== null);
         },
